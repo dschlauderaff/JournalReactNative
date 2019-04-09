6fde69c8eae369ffd0ac099176c2be2802ed1fa5
Dave Schlauderaff <dschlaud@gmail.com>
2019-04-09 13:06:33 -0500

    add loading check to api, display posts as list mapped to props

--- App.js
+++ App.js
@@ -41,10 +41,7 @@ class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#eee",
-    alignItems: "center",
-    justifyContent: "center"
   }
 });
 

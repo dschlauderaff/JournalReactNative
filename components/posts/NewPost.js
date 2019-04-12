f6a7a947ec5fe1055d07b9a8f615f3388dff405e
Dave Schlauderaff <dschlaud@gmail.com>
2019-04-11 19:55:08 -0500

    post newPost to backend as mutation, styling for PostForm

--- components/posts/NewPost.js
+++ components/posts/NewPost.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 import navStyles from "../../styles/navStyles";
 import PostForm from "./PostForm";
 
-export default class NewPost extends Component {
+class NewPost extends Component {
   static navigationOptions = {
     title: "Post",
     ...navStyles
   };
 
-  newPost = ({title, body}) => {
-
-    console.log(title, body)
-  }
+  newPost = ({ title, body }) => {
+    console.log(title, body);
+    this.props
+      .newPost({
+        variables: {
+          title,
+          body
+        }
+      })
+      .then(() => {})
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   render() {
     return (
@@ -23,3 +35,15 @@ export default class NewPost extends Component {
     );
   }
 }
+
+const newPost = gql`
+  mutation newPost($title: String!, $body: String!) {
+    createPost(title: $title, body: $body) {
+      id
+    }
+  }
+`;
+
+export default graphql(newPost, {
+  name: "newPost"
+})(NewPost);
--- components/posts/PostForm.js
+++ components/posts/PostForm.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { View, TextInput, Button } from "react-native";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default class PostForm extends Component {
-
   state = {
     title: "",
     body: ""
-  }
+  };
 
   submitForm = () => {
     this.props.onSubmit({
@@ -19,10 +18,12 @@ export default class PostForm extends Component {
     return (
       <View>
         <TextInput
+          style={styles.title}
           onChangeText={title => this.setState({ title })}
           value={this.state.title}
         />
         <TextInput
+          style={styles.body}
           onChangeText={body => this.setState({ body })}
           value={this.state.body}
         />
@@ -31,3 +32,17 @@ export default class PostForm extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    height: 40,
+    borderColor: "#333",
+    borderWidth: 1
+  },
+  body: {
+    height: 400,
+    borderColor: "#333",
+    borderWidth: 1,
+    textAlignVertical: "top"
+  }
+});
